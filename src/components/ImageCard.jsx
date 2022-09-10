0d7429b0366f6b4dfbc38a7ec80e9317adbd5e22
Sabuhi0 <sabuhiq0@gmail.com>
2022-09-10 19:36:44 +0400

    Add image card component

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,8 +1,35 @@
 
-const ImageCard = () => {
-  return (
-    <div>ImageCard</div>
-  )
+const ImageCard = ({ image }) => {
+    const tags = image.tags.split(",")
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full h-[260px] object-cover" src={image.webformatURL} alt={image.type} />
+            <div className="px-6 py-4">
+                <h1 className="font-bold text-[#20C997] text-2xl">Photo by {image.user}</h1>
+                <ul>
+                    <li>
+                        <strong>Views: </strong>
+                        {image.wiews}
+                    </li>
+                    <li>
+                        <strong>Downloads: </strong>
+                        {image.downloads}
+                    </li>
+                    <li>
+                        <strong>Likes: </strong>
+                        {image.likes}
+                    </li>
+                </ul>
+            </div>
+            <div className="px-6 py-4">
+                {tags.map((tag, index) => (
+                    <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 mt-1 text-sm font-semibold text-gray-700 mr-2">
+                        #{tag}
+                    </span>
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default ImageCard
+export default ImageCard;

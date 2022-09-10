8ad6a2e07deb59a7efccdc3b87ca3c1d68307191
Sabuhi0 <sabuhiq0@gmail.com>
2022-09-10 19:38:05 +0400

    Add states and api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,29 @@
 import { useEffect, useState } from "react";
+import ImageCard from "./components/ImageCard";
 
 const App = () => {
   const [images, setImages] = useState([]);
   const [isLoading, setISLoading] = useState(true);
   const [term, setTerm] = useState("");
-
+  
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
-  })
+    const REACT_APP_PIXABAY_API_KEY="21115300-b484d5aa3a5ce17fbbdeef09b"
+    fetch(`https://pixabay.com/api/?key=${REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setISLoading(false);
+      })
+      .catch(error => console.log(error))
+  }, []);
   
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" />
-      <div className="px-6 py-4">
-        <h1 className="font-bold text-[#20C997] text-2xl">Photo by Sabuhi Gasimov</h1>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            1000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            2500
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4 flex gap-2">
-        <span className="bg-gray-200 rounded-full px-3 py-1 font-semibold text-gray-600">#tag1</span>
-        <span className="bg-gray-200 rounded-full px-3 py-1 font-semibold text-gray-600">#tag2</span>
-        <span className="bg-gray-200 rounded-full px-3 py-1 font-semibold text-gray-600">#tag3</span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image}/>
+        ))}
       </div>
     </div>
   )
